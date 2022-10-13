7107bcfe746f0e0d3344d7cd96407fd14650c073
Bruno Andrade <brunoan99@gmail.com>
2022-10-13 08:03:56 -0300

    feat: add customer create and list integration in api

---
+++ src/infraestructure/api/routes/customer.route.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express"
+import { CreateCustomerUseCase } from "../../../usecase/customer/create/create.customer.usecase"
+import { ListCustomerUseCase } from "../../../usecase/customer/list/list.customer.usecase"
+import { CustomerRepository } from "../../db/customer/repository/sequelize/customer-repository"
+
+export const customerRoute = express.Router()
+
+customerRoute.post("/", async (req: Request, res: Response) => {
+  const usecase = new CreateCustomerUseCase(new CustomerRepository())
+  try {
+    const customerDto = {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        number: req.body.address.number,
+        city: req.body.address.city,
+        zip: req.body.address.zip,
+      }
+    }
+    const output = await usecase.execute(customerDto)
+    return res.status(200).json(output)
+  } catch (err) {
+    return res.status(500).json(err)
+  }
+})
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListCustomerUseCase(new CustomerRepository())
+  try {
+    const customerDto = {}
+    const output = await usecase.execute(customerDto)
+    return res.status(200).json(output)
+  } catch (err) {
+    return res.status(500).json(err)
+  }
+})

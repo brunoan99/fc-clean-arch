250cb6c83fce42f03815130fa6c8aa8ba8eaf832
Bruno Andrade <brunoan99@gmail.com>
2022-10-13 05:08:52 -0300

    test: add Integration tests to CreateProductUseCase

---
+++ src/usecase/product/create/create.product.usecase.integration.spec.ts
@@ -0,0 +1,65 @@
+import { Sequelize } from "sequelize-typescript"
+import { ProductModel } from "../../../infraestructure/product/repository/sequelize/product"
+import { ProductRepository } from "../../../infraestructure/product/repository/sequelize/product-repository"
+import { CreateProductUseCase } from "./create.product.usecase"
+
+describe('List Product Use Case', () => {
+  let sequelize: Sequelize
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    })
+    sequelize.addModels([ProductModel])
+    await sequelize.sync()
+  })
+
+  afterEach(async () => {
+    await sequelize.close()
+  })
+  
+  test('Should create product', async () => {
+    const productRepo = new ProductRepository()
+    const sut = new CreateProductUseCase(productRepo)
+    const input = {
+      name: "Product1",
+      price: 1
+    }
+    const expectedOutput = {
+      id: expect.any(String),
+      name: "Product1",
+      price: 1
+    }
+    const output = await sut.execute(input)
+    expect(output).toEqual(expectedOutput)
+    const productinRepo = await productRepo.findById(output.id)
+    expect(productinRepo).toBeDefined()
+    expect(productinRepo.name).toEqual(input.name)
+    expect(productinRepo.price).toEqual(input.price)
+  })
+
+  test('Should throw an error when name is missing', async () => {
+    const productRepo = new ProductRepository()
+    const sut = new CreateProductUseCase(productRepo)
+    const input = {
+      name: "",
+      price: 1
+    }
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow("Name is required")
+  })
+
+  test('Should throw an error when price is missing', async () => {
+    const productRepo = new ProductRepository()
+    const sut = new CreateProductUseCase(productRepo)
+    const input = {
+      name: "Product1",
+      price: -1
+    }
+    const output = sut.execute(input)
+    await expect(output).rejects.toThrow("Price is required")
+  })
+})
--- src/usecase/product/list/list.product.usecase.integration.spec.ts
+++ src/usecase/product/list/list.product.usecase.integration.spec.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript"
 import { Product } from "../../../domain/product/entity/product"
+import { ProductFactory } from "../../../domain/product/factories/product-factory"
 import { ProductModel } from "../../../infraestructure/product/repository/sequelize/product"
 import { ProductRepository } from "../../../infraestructure/product/repository/sequelize/product-repository"
 import { ListProductUseCase } from "./list.product.usecase"
@@ -25,21 +26,21 @@ describe('List Product Use Case', () => {
   test('Should list products', async () => {
     const productRepo = new ProductRepository()
     const sut = new ListProductUseCase(productRepo)
-    const product1 = new Product("123", "Product1", 1)
+    const product1 = ProductFactory.creatProductA("Product1", 1)
     await productRepo.create(product1)
-    const product2 = new Product("321", "Product2", 2)
+    const product2 = ProductFactory.creatProductA("Product2", 2)
     await productRepo.create(product2)
     const input = {}
     const output = await sut.execute(input)
     const expectedOutput = {
       products: [
         {
-          id: "123",
+          id: expect.any(String),
           name: "Product1",
           price: 1
         },
         {
-          id: "321",
+          id: expect.any(String),
           name: "Product2",
           price: 2
         }

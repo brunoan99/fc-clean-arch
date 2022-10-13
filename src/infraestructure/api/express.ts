fcb7d92136c71297403f41ba638d15381e2b5287
Bruno Andrade <brunoan99@gmail.com>
2022-10-13 05:53:22 -0300

    chore: add express, dotenv and nodemon to project

---
+++ src/infraestructure/api/express.ts
@@ -0,0 +1,20 @@
+import express, { Express }  from "express"
+import { Sequelize } from "sequelize-typescript"
+import { CustomerModel } from "../db/customer/repository/sequelize/customer"
+
+export const app: Express = express()
+app.use(express.json())
+
+export let sequelize: Sequelize
+
+async function setupDb() {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
+  })
+  sequelize.addModels([CustomerModel])
+  await sequelize.sync()
+}
+
+setupDb()
